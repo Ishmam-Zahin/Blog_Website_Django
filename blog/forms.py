25178f610ff6861ed2043957aa092bbfe64554af
ishmam-zahin <ishmamzahin404@gmail.com>
2025-03-15 21:35:44 +0600

    created comment mdoel

--- blog/forms.py
+++ blog/forms.py
@@ -5,6 +5,6 @@ class EmailPostForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(
-    required=False,
-    widget=forms.Textarea
+        required=False,
+        widget=forms.Textarea
     )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm
@@ -56,11 +57,30 @@ def post_share(request, post_id):
         id=post_id,
         status=Post.Status.PUBLISHED
     )
+    sent = False
 
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
     else:
         form = EmailPostForm()
     
@@ -70,5 +90,6 @@ def post_share(request, post_id):
         {
             'post': post,
             'form': form,
+            'sent': sent,
         }
     )
