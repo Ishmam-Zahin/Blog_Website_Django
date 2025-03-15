b36a46c7333caf504f7518be90b5c4237986349f
ishmam-zahin <ishmamzahin404@gmail.com>
2025-03-15 13:28:35 +0600

    added generic class

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.conf import settings
 
 class PublishedManager(models.Manager):
@@ -15,7 +16,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date = 'publish',
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -39,6 +43,18 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args = [
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
+    
 
 
     def __str__(self):
